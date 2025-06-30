4b2b488380001483d9f7a5b3ca17300c48b5a8bf
Sidd2712 <siddharth.2727goyal@gmail.com>
2025-06-30 22:01:49 +0530

    first commit

---
+++ Server/server.js
@@ -0,0 +1,68 @@
+const express=require("express"); 
+const connectDB=require("./configs/connection");
+const dotenv=require("dotenv");
+const cors=require("cors");
+const sendMail=require("./services/mailSender")
+const filesRouter=require("./routes/fileUploadRoutes.js");
+const ExpiryCron=require("./services/cronJob.js");
+const downloadRouter=require('./routes/fileDownloadRoutes.js');
+
+const app=express();
+dotenv.config();
+ExpiryCron();
+const port=process.env.PORT||3000;
+
+// connection to monogDB
+connectDB();
+
+//middleware
+app.use(express.json());
+// app.use(sanitizeInput);
+// app.use(limiter);
+// app.use(cors());
+
+//Routes
+// app.use('/sid', urlRoutes);
+
+
+app.get("/", (req,res)=>{
+    res.send("Sharing App");
+});
+
+let emailOptions={
+    emailTo:"siddharth.2727goyal@gmail.com",
+    emailFrom:"goyalsiddharth81@gmail.com",
+    link: "asdf",
+    fileName:"asdf",
+    size:12
+}
+
+app.use('/',filesRouter);
+
+app.use('/',downloadRouter);
+// app.get("/send",(req,res)=>{
+//     sendMail()
+// })
+// error handler(404)
+// app.use((req,res,next)=>{
+//     res.status(404).json({
+//         error:{
+//             message: "Route not found"
+//         }
+//     });
+// });
+
+// error handling middleware
+// app.use((err,req,res,next)=>{
+//     console.error(err.stack);
+//     res.status(err.status || 500).json({
+//         error:{
+//             message: err.message || "Internal server Error"
+//         }
+//     });
+// });
+
+app.listen(port,()=>{
+    
+    console.log(`Server is running on port ${port}`);
+});
